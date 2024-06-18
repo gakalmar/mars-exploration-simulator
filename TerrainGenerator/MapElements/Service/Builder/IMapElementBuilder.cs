805d99c49edcb7884a8dba6818115c6183ccade7
Gabor Kalmar <gakalmar@gmail.com>
2024-06-18 16:14:11 +0200

    Add Builder classes

---
+++ TerrainGenerator/MapElements/Service/Builder/IMapElementBuilder.cs
@@ -0,0 +1,8 @@
+﻿using TerrainGenerator.MapElements.Model;
+
+namespace TerrainGenerator.MapElements.Service.Builder;
+
+public interface IMapElementBuilder
+{
+    MapElement Build(int size, string symbol, string name, int dimensionGrowth, string? preferredLocationSymbol = null);
+}
