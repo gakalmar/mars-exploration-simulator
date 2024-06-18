dab408cc2402ba0f560c696f0d24a770ae28c4f9
Gabor Kalmar <gakalmar@gmail.com>
2024-06-18 17:16:08 +0200

    Add Repository and DB

---
+++ TerrainExplorer/Repository/ExplorationRepository.cs
@@ -0,0 +1,56 @@
+﻿using TerrainExplorer.Exploration;
+
+namespace TerrainExplorer.Repository;
+
+public class ExplorationRepository : IExplorationRepository
+{
+    private readonly string _dbFilePath;
+
+    public ExplorationRepository(string dbFilePath)
+    {
+        _dbFilePath = dbFilePath;
+    }
+    
+    // Establish connection:
+    private SqliteConnection GetPhysicalDbConnection()
+    {
+        var dbConnection = new SqliteConnection($"Data Source ={_dbFilePath};Mode=ReadWrite");
+        dbConnection.Open();
+        return dbConnection;
+    }
+    
+    private void ExecuteNonQuery(string query)
+    {
+        using var connection = GetPhysicalDbConnection();
+        using var command = GetCommand(query, connection);
+        command.ExecuteNonQuery();
+    }
+    
+    private static SqliteCommand GetCommand(string query, SqliteConnection connection)
+    {
+        return new SqliteCommand
+        {
+            CommandText = query,
+            Connection = connection,
+        };
+    }
+
+    // Public methods:
+    public void Add(int steps, int mineralsFound, int waterFound, ExplorationOutcome outcome)
+    {
+        var query = $"INSERT INTO Summaries (Created, Steps, Water, Mineral, Outcome) " +
+                    $"VALUES (" +
+                    $"'{DateTime.Now}'," +
+                    $" '{steps}'," +
+                    $" '{waterFound}'," +
+                    $" '{mineralsFound}'," +
+                    $" '{outcome.ToString()}')";
+        ExecuteNonQuery(query);
+    }
+
+    public void DeleteAll()
+    {
+        var query = "DELETE FROM Summaries";
+        ExecuteNonQuery(query);
+    }
+}
---
+++ TerrainExplorer/Repository/IExplorationRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace TerrainExplorer.Repository;
+
+public interface IExplorationRepository
+{
+    void Add(int steps, int mineralsFound, int waterFound, ExplorationOutcome outcome);
+    void DeleteAll();
+}
