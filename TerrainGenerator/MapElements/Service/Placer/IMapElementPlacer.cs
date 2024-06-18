dc5c3766b9c40007989fcee25e9913c15c494b8a
Gabor Kalmar <gakalmar@gmail.com>
2024-06-18 16:14:51 +0200

    Add Placer classes

---
+++ TerrainGenerator/MapElements/Service/Placer/IMapElementPlacer.cs
@@ -0,0 +1,10 @@
+﻿using TerrainGenerator.Calculators.Model;
+using TerrainGenerator.MapElements.Model;
+
+namespace TerrainGenerator.MapElements.Service.Placer;
+
+public interface IMapElementPlacer
+{
+    bool CanPlaceElement(MapElement element, string?[,] map, Coordinate coordinate);
+    void PlaceElement(MapElement element, string?[,] map, Coordinate coordinate);
+}
