2f40685009e55295d2571f8bd2cdff7804abe249
Gabor Kalmar <gakalmar@gmail.com>
2024-06-18 16:16:33 +0200

    Add Output classes

---
+++ TerrainGenerator/Output/Service/IMapFileWriter.cs
@@ -0,0 +1,8 @@
+﻿using TerrainGenerator.MapElements.Model;
+
+namespace TerrainGenerator.Output.Service;
+
+public interface IMapFileWriter
+{
+    void WriteMapFile(Map map, string file);
+}
---
+++ TerrainGenerator/Output/Service/MapFileWriter.cs
@@ -0,0 +1,11 @@
+﻿using TerrainGenerator.MapElements.Model;
+
+namespace TerrainGenerator.Output.Service;
+
+public class MapFileWriter : IMapFileWriter
+{
+    public void WriteMapFile(Map map, string file)
+    {
+        File.WriteAllText(file, map.ToString());
+    }
+}
